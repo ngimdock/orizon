ff4f2d300fa85ee18ba2ed0e42f7dc418cfd229b
ngimdock <ngimdockzemfack@gmail.com>
2021-07-24 17:22:44 +0100

    amélioration du style du site

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 
-
 const checkMenu = document.querySelector(".check-menu");
-const pageMenu = document.querySelector(".page-menu");
 
 checkMenu.onclick = function() {
+	const pageMenu = document.querySelector(".page-menu");
 	pageMenu.classList.toggle("show-page-menu")
 	this.classList.toggle("active");
+
+	const boxMenu = document.querySelector(".box");
+	const lines = document.querySelectorAll(".ligne");
+	if(boxMenu.classList.contains('dark-boxMenu') && this.classList.contains('active')){
+		boxMenu.classList.remove('dark-boxMenu');
+		lines.forEach(line => line.classList.remove('white-line'))
+	}else{
+		boxMenu.classList.add('dark-boxMenu');
+		lines.forEach(line => line.classList.add('white-line'))
+	}
+
+};
+
+/*const scrollWindow = () => {
+	window.addEventListener("scroll", () => {
+		console.log(window.scrollY);
+	});
+};
+
+scrollWindow();*/
+
+window.onscroll = function(event) {
+	
+	const boxMenu = document.querySelector(".box");
+	const lines = document.querySelectorAll(".ligne");
+	if(this.scrollY > 80){
+		boxMenu.classList.add("dark-boxMenu");
+		lines.forEach(line  => line.classList.add("white-line"))
+	}else{
+		boxMenu.classList.remove("dark-boxMenu");
+		lines.forEach(line  => line.classList.remove("white-line"))
+	}
 };
